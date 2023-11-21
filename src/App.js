d0ce4dbebc7a0ed5ec222f091604d32106e3b8c1
Kumari Thotapalli <t.kumari59@gmail.com>
2023-11-21 16:11:25 +0100

    read me

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { Container, Row, Col, Table} from 'react-bootstrap';
 const UserProfile = ({ props }) => {
    const {uid} = useParams()
 
-
    // Defautl values
    const userProfileInfo = {
     firstName: '',
@@ -28,10 +27,9 @@ const UserProfile = ({ props }) => {
   const [userIntersts, setUserIntersts] = useState(userInterstsDefaultVaules);
 
   useEffect(() => {
-    // Function to fetch data from the API
     const fetchData = async () => {
       try {
-        const response = await fetch('/users/'+uid); // Replace with your API endpoint
+        const response = await fetch('/users/'+uid); 
         const result = await response.json();
       
         setUserProfile(result);
@@ -47,7 +45,6 @@ const UserProfile = ({ props }) => {
         console.error('Error fetching data:', error);
       }
     };
-    
    fetchData();
   }, []); 
 
@@ -75,7 +72,6 @@ const UserProfile = ({ props }) => {
 }
 
 const Home = ({ props }) => {
-
   return (
     <div className="table-responsive">
     <h4>User list</h4>
@@ -87,38 +83,37 @@ const Home = ({ props }) => {
         </tr>
       </thead>
       <tbody>
-        
           <tr >
             <td>User 1</td>
             <td> <Link to={`/profile/1`}>View Profile</Link></td>
           </tr>
-        
+          <tr >
+            <td>User 2</td>
+            <td> <Link to={`/profile/2`}>View Profile</Link></td>
+          </tr>
+          <tr >
+            <td>User 3</td>
+            <td> <Link to={`/profile/3`}>View Profile</Link></td>
+          </tr>
+          <tr >
+            <td>User 4</td>
+            <td> <Link to={`/profile/4`}>View Profile</Link></td>
+          </tr>
       </tbody>
     </Table>
   </div>
   );
-
-
 }
 
-
-
-
-// Main App
 const App = () => {
   return (
     <Router>
     <Routes>
       <Route exact path="/profile/:uid"  element={<UserProfile />}  />
       <Route exact path="/"  element={<Home />}  />
-
     </Routes>
     </Router>
-
   );
 };
-
-
-
 
 export default App;
